97f5160df3fa8c67e17986a854cf3515cb7bddf1
BOloo <bicosteve4@gmail.com>
2023-12-26 17:38:29 +0300

    Added mysql configs

---
+++ db/db.go
@@ -0,0 +1,60 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+
+type DB struct {
+	DB *sql.DB
+}
+
+var dbConnection = &DB{}
+
+const maxOpenDbConnections = 10
+const maxIdleDbConn = 5
+const maxDbLifetime = 5 * time.Minute
+
+
+
+func ConnectMysql(dsn string)(*DB,error){
+	db, err := sql.Open("mysql",dsn)
+
+	if err != nil {
+		return nil, err 
+	}
+
+	db.SetMaxOpenConns(maxOpenDbConnections)
+	db.SetConnMaxIdleTime(maxIdleDbConn)
+	db.SetConnMaxLifetime(maxDbLifetime)
+
+	err = testDB(db)
+
+	if err != nil {
+		return nil, err 
+	}
+
+	dbConnection.DB = db 
+
+	return dbConnection,nil 
+}
+
+
+func testDB(d *sql.DB)error {
+
+	err := d.Ping()
+
+	if err != nil {
+		fmt.Printf("Error %v ",err)
+		return err 
+	}
+
+	fmt.Println("DB pinged successufly")
+
+	return nil 
+
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module donateapp
 
 go 1.18
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/joho/godotenv v1.5.1
+
+require github.com/go-sql-driver/mysql v1.7.1
