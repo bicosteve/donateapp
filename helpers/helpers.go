ffa10e6811789f68bc05d5143fc66cd5f92936b5
BOloo <bicosteve4@gmail.com>
2023-12-27 04:34:57 +0300

    Added Json helpers

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"donateapp/models"
 	"encoding/json"
 	"errors"
 	"log"
@@ -44,5 +45,63 @@ func ReadJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
 	}
 
 	return nil
-
 }
+
+func WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ErrorJSON(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload models.JsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	err = WriteJSON(w, statusCode, payload)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//// DB Helper
+//func UserExists(db *sql.DB, user User) (bool, error) {
+//	row := db.QueryRow("SELECT id FROM users WHERE email LIKE %?%", user.Email)
+//	var id int64
+//
+//	err := row.Scan(&id)
+//
+//	if err == sql.ErrNoRows {
+//		return false, nil
+//	}
+//
+//	if err != nil {
+//		return false, err
+//	}
+//
+//	return true, nil
+//}
